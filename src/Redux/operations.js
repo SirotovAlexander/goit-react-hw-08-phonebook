e0fbe12b27133ef55c25933cb87b053005889f84
SirotovAlexander <alexander.sirotov@gmail.com>
2023-08-17 01:52:58 +0300

    Update operations.js

--- src/Redux/operations.js
+++ src/Redux/operations.js
@@ -1,30 +1,45 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-export const fetchContacts = createAsyncThunk('contacts/fetchAll', async () => {
-  const response = await axios.get(
-    'https://64d753752a017531bc1323d8.mockapi.io/contacts'
-  );
-  return response.data;
-});
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetchAll',
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get(
+        'https://64d753752a017531bc1323d8.mockapi.io/contacts'
+      );
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
 
 export const deleteContacts = createAsyncThunk(
   'contacts/deleteContact',
-  async id => {
-    const response = await axios.delete(
-      `https://64d753752a017531bc1323d8.mockapi.io/contacts/${id}`
-    );
-    return response.data;
+  async (id, thunkAPI) => {
+    try {
+      const response = await axios.delete(
+        `https://64d753752a017531bc1323d8.mockapi.io/contacts/${id}`
+      );
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
   }
 );
 
 export const addContacts = createAsyncThunk(
   'contacts/addContacts',
-  async contact => {
-    const response = await axios.post(
-      `https://64d753752a017531bc1323d8.mockapi.io/contacts/`,
-      contact
-    );
-    return response.data;
+  async (contact, thunkAPI) => {
+    try {
+      const response = await axios.post(
+        `https://64d753752a017531bc1323d8.mockapi.io/contacts/`,
+        contact
+      );
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
   }
 );
