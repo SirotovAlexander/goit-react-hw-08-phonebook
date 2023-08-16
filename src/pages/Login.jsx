8b53b4b826102a863dc21bce001d8d2a56ce5b64
SirotovAlexander <alexander.sirotov@gmail.com>
2023-08-17 01:30:29 +0300

    add form
    add login-form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,11 @@
 import { Container } from 'components/container/Container';
-import { Section } from 'components/section/Section';
 
-import css from '../components/phonebookform/PhoneBookForm.module.css';
+import css from './Login.module.css';
 
 export const Login = () => {
   return (
     <Container>
-      <Section>
+      <section className={css.section}>
         <div className={css.formwrapper}>
           <form className={css.useagecard}>
             <label className={css.useagecard__lable}>E-Mail</label>
@@ -16,7 +15,6 @@ export const Login = () => {
               type="text"
               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              placeholder="Please enter a name"
               required
             />
             <label className={css.useagecard__lable}>Password</label>
@@ -26,7 +24,6 @@ export const Login = () => {
               type="tel"
               pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
               title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              placeholder="Please enter a number"
               required
             />
             <button type="submit" className={css.useagecard__button}>
@@ -34,7 +31,7 @@ export const Login = () => {
             </button>
           </form>
         </div>
-      </Section>
+      </section>
     </Container>
   );
 };
