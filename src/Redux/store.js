c2500a051fc8044be31412aca86b329b2096c557
SirotovAlexander <alexander.sirotov@gmail.com>
2023-08-19 01:45:07 +0300

    add persistor
    add persistor

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,11 +1,39 @@
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import { ContactsSliceReduser } from './ContactsSlice/ContactsSlice';
 import { FilterReduser } from './FilterSlice/FilterSlice';
+import { authReducer } from './AuthSlice/AuthSlice';
+
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
 
 export const store = configureStore({
   reducer: {
     contacts: ContactsSliceReduser,
     filter: FilterReduser,
+    auth: persistReducer(authPersistConfig, authReducer),
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  devTools: process.env.NODE_ENV === 'development',
 });
+
+export const persistor = persistStore(store);
