1ab01cd574e7e6e0477a92b711a7f35e0f380a31
SirotovAlexander <alexander.sirotov@gmail.com>
2023-08-19 02:29:03 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,39 +8,6 @@ import { ErrorPage } from 'pages/ErrorPage';
 import { PublicRoute } from './PublicRoute';
 import { PrivateRoute } from './PrivateRoute';
 
-// import { Container } from './container/Container';
-// import { Section } from './section/Section';
-// import { Loader } from './Loader/Loader';
-
-// import PhoneBookForm from './phonebookform/PhoneBookForm';
-// import Contacts from './contacts/Contacts';
-// import Filter from './filter/Filter';
-// import { useSelector } from 'react-redux';
-// import { selectLoading, selectError } from '../Redux/selectors';
-
-// export const App = () => {
-//   const isLoading = useSelector(selectLoading);
-//   const isError = useSelector(selectError);
-
-//   if (isError) Notiflix.Notify.warning(`${isError.message}`);
-//   return (
-//     <Container>
-//       {isLoading && <Loader />}
-
-//       <Section>
-//         <h1>Phonebook</h1>
-//         <PhoneBookForm />
-//         {!isError && (
-//           <>
-//             <h2>Contacts</h2>
-//             <Filter />
-//             <Contacts />
-//           </>
-//         )}
-//       </Section>
-//     </Container>
-//   );
-// };
 export const App = () => {
   return (
     <Routes>
