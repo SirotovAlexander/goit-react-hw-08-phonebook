fac25534cb26a453cc648ea5a07e8ff162ac40ba
SirotovAlexander <alexander.sirotov@gmail.com>
2023-08-19 01:15:40 +0300

    Update selectors.js

--- src/Redux/selectors.js
+++ src/Redux/selectors.js
@@ -1,9 +1,9 @@
 import { createSelector } from '@reduxjs/toolkit';
 
 export const selectContacts = state => state.contacts.items;
-export const selectFilterValue = state => state.filter;
 export const selectLoading = state => state.contacts.loading;
 export const selectError = state => state.contacts.error;
+export const selectFilterValue = state => state.filter;
 
 export const selectVisibleContacts = createSelector(
   [selectContacts, selectFilterValue],
